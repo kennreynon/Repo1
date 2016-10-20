597f3d746c131ab2d99426f05a1690ccf1c7d0eb
kennreynon <kennedyreynon@gmail.com>
2016-10-20 20:58:53 +0800

    Adding prokj1.java

---
+++ proj1.java
@@ -0,0 +1,19 @@
+public class FinalProject{
+	public static void main(String args){
+		public static int add(int x,int y){
+			return x+y;
+		}
+		
+		public static int subtract(int x,int y){
+			return x-y;
+		}
+		
+		public static int multiply(int x,int y){
+
+		}
+		
+		public static int divide(int x,int y){
+			
+		}
+	}
+}
